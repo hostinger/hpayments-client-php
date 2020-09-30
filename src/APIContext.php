c84dc6c107a58130c7e1ffe4c7f3733a0e818f5b
Aurimas Rekstys <aurimas.rekstys@hostinger.com>
2020-09-30 09:59:57 +0300

    update version

--- src/APIContext.php
+++ src/APIContext.php
@@ -14,7 +14,7 @@ class APIContext
     /**
      * @var string CLIENT_VERSION
      */
-    const CLIENT_VERSION = '1.1.1';
+    const CLIENT_VERSION = '1.2.0';
 
     /**
      * @var string REQUEST_OPTION_HTTP_ERRORS
